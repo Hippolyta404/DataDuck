08939dccb9a468acd4d19d4adbf176b7ba10719e
Hippolyta404 <Hippolyta404@Gmail.com>
2022-03-16 15:17:50 +0000

    File structure and home page

---
+++ FrontEnd/home.php
@@ -0,0 +1,16 @@
+<?php include("/includes/config.php");?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include("/includes/head-tag-contents.php");?>
+    </head>
+    <body>
+        <?php include("/includes/design-top.php");?>
+        <?php include("/includes/navigation.php");?>
+        <div class = "container" id="bulk">
+            <p>
+                Sample
+            </p>
+        </div>
+    </body>
+</html>
